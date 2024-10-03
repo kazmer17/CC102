a3f85ef69370df50baec54eaab9c87abd1538c09
kazmer17 <kazmerfuensalida7@gmail.com>
2024-10-03 10:35:00 +0800

    Add files via upload

---
+++ 02Basicoutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Our lady of Lourdes Collage Foundation" << endl;
+	cout << "Computer Studies" << endl;
+	cout << "BSIT 1B" << endl;
+	cout << "Kazmer Fuensalida" << endl;
+	cout << "Purok 5 Parang Jose Panganiban Camarines Norte" << endl;
+	
+	return 0;
+}
