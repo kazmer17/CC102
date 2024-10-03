e614869f6fc2521033b0ec796ee3b26f1ff48da2
kazmer17 <kazmerfuensalida7@gmail.com>
2024-10-03 11:33:50 +0800

    Add files via upload

---
+++ 04BasicInput.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nN;
+	string CYL;
+	string sch;
+	
+	cout<<"nickname:\t";
+	cin >>nN;
+	cout <<"Course-Year Level:\t";
+	cin >> CYL;
+	cout<<"School:\t";
+	cin>>sch;
+	cout<<"\nWow Congrats "<<nN<<"! "<<CYL<<" is a nice course. And You are Studying in "<<sch<<" which is one of the Center of Excellence in Tertiary Education.";
+	
+    return 0;
+}
+
+
