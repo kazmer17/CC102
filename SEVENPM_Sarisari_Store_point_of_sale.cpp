ec0e64d8ea25450b8ca01ebb7cd12047047e9938
kazmer17 <kazmerfuensalida7@gmail.com>
2024-10-15 10:34:25 +0800

    Add files via upload

---
+++ SEVENPM_Sarisari_Store_point_of_sale.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string nickname; 
+	float sugarPriceUSD, ricePricePound, sardinesPricePound, coffeePriceUSD, milkPriceUSD;
+	float USDtoPHP, PoundtoPHP, TotalPHP;
+	float SugarinPHP, RiceinPHP, SardinesinPHP, CoffeeinPHP, MilkinPHP;
+	int sugarQty,riceQty, sardinesQty, coffeeQty, milkQty;
+	
+	cout << "Enter the price of sugar : ";
+	cin >> sugarPriceUSD;
+	cout << "Enter the price of rice : ";
+	cin >> ricePricePound;
+	cout << "Enter the price of sardines : ";
+	cin >> sardinesPricePound;
+	cout << "Enter the price of coffee : ";
+	cin >> coffeePriceUSD;
+	cout << "Enter the price of milk : ";                                                                                                                               
+	cin >> milkPriceUSD;
+	
+	cout << "Enter the quantity of sugar : ";
+	cin >> sugarQty;
+	cout << "Enter the quantity  of rice : ";
+	cin >> riceQty;
+	cout << "Enter the quantity  of sardines :";
+	cin >> sardinesQty;
+	cout << "Enter the quantity  of coffee : ";
+	cin >> coffeeQty;
+	cout << "Enter the quantity  of milk : ";
+	cin >> milkQty;
+	 
+
+	SugarinPHP = sugarPriceUSD * USDtoPHP * sugarQty;
+	RiceinPHP = ricePricePound * PoundtoPHP * riceQty;
+	SardinesinPHP = sardinesPricePound * sardinesQty;
+	CoffeeinPHP = coffeePriceUSD * USDtoPHP * coffeeQty;
+	MilkinPHP = milkPriceUSD * USDtoPHP * milkQty;
+	
+	TotalPHP = SugarinPHP + RiceinPHP + SardinesinPHP + CoffeeinPHP + MilkinPHP;
+	
+	cout << "Total price in PHP: " << TotalPHP << endl;
+	
+	return 0;
+}
